52f69ac88f66e7cb02ff63c3a5e8170e3fb5c488
Dan Buch <dan@meatballhat.com>
2020-11-17 10:45:09 -0500

    Fill in manifest-only implementation of connect-publish

--- connect-publish/src/connect-publish.ts
+++ connect-publish/src/connect-publish.ts
@@ -1,25 +1,67 @@
 import { URL } from 'url'
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 class ActionArgs {
   public apiKey: string = ''
-  public directory: string = ''
+  public dirs: string[] = []
   public serverName: string = ''
-  public contentType: string = ''
   public url: string = ''
 }
 
-export async function connectPublish (args: ActionArgs): Promise<void> {
-  // TODO: ensure server and then publish
+export async function connectPublish (args: ActionArgs): Promise<any> {
+  return await addServer(args).then((rc: number) => {
+    if (rc !== 0) {
+      throw new Error('non-zero exit from rsconnect add')
+    }
+  })
+    .then(async () => await publishFromDirs(args.dirs, args.serverName))
+    .then((rcs: number[]) => {
+      if (rcs.some((rc: number) => rc !== 0)) {
+        throw new Error('non-zero exit from at least one publish')
+      }
+    })
+    .catch((err: any) => {
+      core.setFailed(err)
+    })
+}
+
+async function addServer (args: ActionArgs): Promise<number> {
+  return await exec.exec('rsconnect', [
+    'add',
+    '--name', args.serverName,
+    '--server', args.url,
+    '--api-key', args.apiKey
+  ], { silent: true })
+}
+
+async function publishFromDirs (dirs: string[], serverName: string): Promise<number[]> {
+  const ret: number[] = []
+  for (const dir of dirs) {
+    ret.push(await publishFromDir(dir, serverName))
+  }
+  return ret
+}
+
+async function publishFromDir (dir: string, serverName: string): Promise<number> {
+  return await exec.exec('rsconnect', [
+    'deploy',
+    'manifest',
+    '--name', serverName,
+    dir
+  ])
 }
 
 export function loadArgs (): ActionArgs {
-  const rawURL = core.getInput('url', { required: true })
-  const serverName = core.getInput('server-name')
+  let serverName = core.getInput('server-name')
+  if (serverName === '' || serverName === undefined || serverName === null) {
+    serverName = 'default'
+  }
 
   let apiKey = core.getInput('api-key')
   const apiKeySpecified = apiKey !== ''
 
+  const rawURL = core.getInput('url', { required: true })
   const url = new URL(rawURL)
   if (url.password !== '') {
     if (apiKeySpecified) {
@@ -35,13 +77,18 @@ export function loadArgs (): ActionArgs {
   url.password = ''
   url.username = ''
 
+  const dirs = core.getInput('dir').split('\n').map(s => s.trim()).filter(s => s !== '')
+  if (dirs.length === 0) {
+    dirs.push('.')
+  }
+
   const args = new ActionArgs()
 
   args.apiKey = apiKey
-  args.contentType = core.getInput('type')
-  args.directory = core.getInput('directory')
+  args.dirs = dirs
+  args.serverName = serverName
   args.url = url.toString()
-  args.serverName = serverName ?? 'default'
 
+  console.log('args=%o', args)
   return args
 }
